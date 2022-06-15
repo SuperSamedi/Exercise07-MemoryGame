fc52fd91f1db823c2a8a975b69b37a6d2384dbf4
Max <contact.max.dolmans@gmail.com>
2022-06-15 09:17:29 +0200

    Board population

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const numOfSets = 3;
+const boardHTML = document.getElementById("board");
+const sets = ["plains", "island", "swamp", "mountain", "forest"];
 const cardsPerSet = 2;
+
+//Populate board
+for (let i = 0; i < sets.length; i++) {
+    for (let j = 0; j < cardsPerSet; j++) {
+        const newCard = document.createElement("input");
+        newCard.type = "button";
+        newCard.classList.add("card");
+        newCard.classList.add(sets[i])
+        newCard.value = "";
+        newCard.addEventListener("click", onCardClick);
+        boardHTML.appendChild(newCard);
+    }
+}
+
+function onCardClick(){
+
+}
